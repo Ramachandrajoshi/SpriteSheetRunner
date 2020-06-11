710439d9c683289755e1883c34d8da9eb913bbbc
Ramachandra Joshi <ramueargoppa@gmail.com>
2020-06-11 22:17:43 +0530

    Sprites with equal size and centered drawing will be best fit

---
+++ README.md
@@ -0,0 +1,3 @@
+Create 2d FPS animations using Sprite Sheets.
+
+you can run the sprite sheet FPS animation with just from xml layout
---
+++ spriterunner/src/main/java/com/codyzen/spriterunner/SpriteView.kt
@@ -0,0 +1,93 @@
+package com.codyzen.spriterunner
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.graphics.drawable.toBitmap
+import java.util.*
+import kotlin.concurrent.schedule
+
+
+class SpriteView(context: Context,attrs: AttributeSet?) : View(context, attrs) {
+    var image: Bitmap? = null
+    var spriteWidth = 0.0
+    var spriteHeight = 0.0
+    var columns = 4
+    var rows = 4
+    var fps = columns * rows
+    var lastFrame = fps
+    var isFixedRow = false
+
+    var renderRow = 0
+    var renderColumn = 0
+    private val srcFrame = Rect()
+    private val dstFrame = Rect()
+    private var timer = Timer()
+
+
+
+    init {
+        if (attrs != null){
+            val a = context.obtainStyledAttributes(
+                attrs, R.styleable.SpriteView, 0, 0
+            )
+            image=a.getDrawable(R.styleable.SpriteView_src)?.toBitmap()
+            columns = a.getInt(R.styleable.SpriteView_columns,columns)
+            rows = a.getInt(R.styleable.SpriteView_rows,rows)
+            fps = a.getInt(R.styleable.SpriteView_fps,fps)
+            lastFrame = a.getInt(R.styleable.SpriteView_lastFrame,lastFrame)
+            renderRow = a.getInt(R.styleable.SpriteView_renderRow,renderRow)
+            isFixedRow = a.getBoolean(R.styleable.SpriteView_isFixedRow,isFixedRow)
+            start()
+        }
+    }
+
+    constructor(context: Context) : this(context,null)
+
+
+
+    fun start() {
+        stop()
+        if (image != null) {
+            spriteWidth = image!!.width.toDouble() / columns
+            spriteHeight = image!!.height.toDouble() / rows
+        }
+        timer.schedule(0, (1000 / fps).toLong()) {
+            invalidate()
+        }
+    }
+
+    fun stop(){
+        timer.cancel()
+        timer = Timer()
+    }
+    override fun onDraw(canvas: Canvas?) {
+        if (image != null) {
+            val srcX = renderColumn * spriteWidth
+            val srcY = renderRow * spriteHeight
+            srcFrame.left = srcX.toInt()
+            srcFrame.top = srcY.toInt()
+            srcFrame.right = (srcX + spriteWidth).toInt()
+            srcFrame.bottom = (srcY + spriteHeight).toInt()
+
+            dstFrame.left = 0
+            dstFrame.top = 0
+            dstFrame.right = width
+            dstFrame.bottom = height
+
+            canvas?.drawBitmap(image!!, srcFrame, dstFrame, null)
+            if(lastFrame == (renderRow * renderColumn)){
+                renderRow = 0
+                renderColumn = 0
+                return
+            }
+            if (renderColumn == columns - 1 && !isFixedRow) {
+                renderRow = ++renderRow % rows
+            }
+            renderColumn = ++renderColumn % columns
+        }
+    }
+}
